a75a1b2abe55dde6fd12d27a2a77c8144bbc9c52
Adrian Lamour <recreate3d@gmail.com>
2024-07-20 16:04:22 +0100

    update build location

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build' // Customize output directory
-  }
+  },
+  base: '/ReactThreeFiber-Biolerplate/' // Replace 'repository-name' with the name of your GitHub repository
+
 })
